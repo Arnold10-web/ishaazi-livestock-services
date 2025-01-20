307f36c05ef6f9f881425cf0211f87ddea4995bc
Arnold10-web <arnoldakandinda@gmail.com>
2025-01-20 17:34:23 +0300

    Removed node_modules and frontend from tracking

--- farming-magazine-frontend/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-// helpers/auth.js
-const localStorage = window.localStorage;
-
-export const getAuthHeader = () => {
-  const token = localStorage.getItem('myAppAdminToken'); // Use standardized key
-  if (!token) throw new Error('Token not found. Please login as admin.');
-  return { Authorization: `Bearer ${token}` };
-};
