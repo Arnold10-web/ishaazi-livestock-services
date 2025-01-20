e3e6eb7a8f2ffa1ac3e76addc48ca5d818c25f97
Arnold10-web <arnoldakandinda@gmail.com>
2025-01-20 21:18:48 +0300

    removed deprecated newyrlparser and unified topology

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true, // Handles connection deprecations
+            
         });
         console.log('MongoDB Connected');
     } catch (err) {
